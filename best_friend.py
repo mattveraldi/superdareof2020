ad27f4f6b7929cdd5ba0e0cfc043081e77bd637a
Matteo Veraldi <mattveraldi@gmail.com>
2019-11-11 17:35:30 +0100

    Update best_friend.py

--- best_friend.py
+++ best_friend.py
@@ -6,9 +6,10 @@ class BestFriendSpider(scrapy.Spider):
     start_urls = [url]
     def parse(self, response):
         xp = ".correct"
-        clean_res = response.css(xp).xpath("@value").extract()
+        #clean_res = response.css(xp).xpath("@value").extract() answers as letters a, b, c, d
+        clean_res = response.css(xp).xpath("div/text()").extract() #answers as answer text
         counter = 0
         for unicod in clean_res:
             counter += 1
-            print "Risposta numero " + str(counter) + ": " + unicod.encode("ascii", "ignore")
-
+            #print correct answers
+            print "Risposta numero " + str(counter) + ": " + unicod.encode("ascii", "replace")
