7f42b569862f8f52f42adbdfcc509973ad1a8488
Matteo Veraldi <mattveraldi@gmail.com>
2019-11-17 13:15:37 +0100

    Update best_friend.py

--- best_friend.py
+++ best_friend.py
@@ -6,8 +6,7 @@ class BestFriendSpider(scrapy.Spider):
     start_urls = [url]
     def parse(self, response):
         css_class = ".correct"
-        #clean_res = response.css(xp).xpath("@value").extract() answers as letters a, b, c, d
-        clean_res = response.css(css_class).xpath("div/text()").extract() #answers as answer text
+        clean_res = response.css(css_class).xpath("div/text()").extract()
         counter = 0
         for unicod in clean_res:
             counter += 1
